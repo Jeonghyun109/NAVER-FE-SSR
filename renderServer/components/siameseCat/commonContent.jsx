95391e987b4cedaf8c4d05f3d83d6c66a0fd9cd8
itemgiver <williamgyuhosuh@naver.com>
2021-11-18 17:29:08 +0900

    Add: SiameseCat Image

--- renderServer/components/siameseCat/commonContent.jsx
+++ renderServer/components/siameseCat/commonContent.jsx
@@ -5,10 +5,30 @@ function CommonContent(props) {
   return (
     <CommonContentArea>
       <PhotoList>
-        <MainPhoto></MainPhoto>
+        <MainPhoto>
+          <LinkMainPhoto>
+            <img
+              src="https://search.pstatic.net/common?type=f&amp;size=518x522&amp;quality=95&amp;direct=true&amp;src=http%3A%2F%2Fshop1.phinf.naver.net%2F20210110_224%2F1610262906543iYaiJ_JPEG%2F46398515690451726_622079393.jpeg"
+              style={{
+                width: "260.6px",
+                height: "260.6px",
+                borderRadius: "8px 0 0 0",
+              }}
+            />
+          </LinkMainPhoto>
+        </MainPhoto>
         <SmallPhoto>1</SmallPhoto>
         <SmallPhoto>2</SmallPhoto>
-        <SmallPhoto>3</SmallPhoto>
+        <SmallPhoto>
+          <img
+            src="https://search.pstatic.net/common?type=f&amp;size=260x260&amp;quality=95&amp;direct=true&amp;src=http%3A%2F%2Fshop1.phinf.naver.net%2F20200401_12%2F1585730100281Sa5Vj_JPEG%2F21865709587741555_647870209.jpg"
+            style={{
+              width: "129.8px",
+              height: "129.8px",
+              borderRadius: "0 8px 0 0",
+            }}
+          />
+        </SmallPhoto>
         <SmallPhoto>4</SmallPhoto>
         <SmallPhoto>5</SmallPhoto>
         <SmallPhoto>6</SmallPhoto>
@@ -35,9 +55,7 @@ const PhotoList = styled.ul`
 const MainPhoto = styled.li`
   width: 260.6px;
   height: 260.6px;
-  border-radius: 8px 0 0 0;
   margin: 1px 0 0 1px;
-  background-color: green;
   grid-row-start: 1;
   grid-row-end: 3;
 `;
@@ -46,7 +64,12 @@ const SmallPhoto = styled.li`
   width: 129.8px;
   height: 129.8px;
   margin: 1px 0 0 1px;
-background-color: red;
+  background-color: red;
+`;
+
+const LinkMainPhoto = styled.a`
+  display: block;
+  cursor: pointer;
 `;
 
 export default CommonContent;
