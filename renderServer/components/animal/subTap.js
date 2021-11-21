6dcba708b1e58e2206b927f19177532ff7e5cf65
itemgiver <williamgyuhosuh@naver.com>
2021-11-21 13:55:36 +0900

    Fix: remove duplicate code in animal/subTap

--- renderServer/components/animal/subTap.js
+++ renderServer/components/animal/subTap.js
@@ -2,32 +2,26 @@ import React from "react";
 import styles from "./subTap.module.scss";
 
 function SubTap(props) {
+  const subTapArray = ["기본정보", "포토", "함께 찾은 품종"];
+
   return (
     <div className={styles.subTapArea}>
       <div className={styles.scrollMainTap}>
         <ul className={styles.tapList}>
           <li className={styles.tapEntire}>
-            <a className={styles.linkEntire}>
+            <a className={styles.tapEntireLink}>
               <span className={styles.subTapText}>전체</span>
             </a>
           </li>
-          <li className={styles.tapBasicInfo}>
-            <a className={styles.linkBasicInfo}>
-              <span className={styles.subTapText}>기본정보</span>
-            </a>
-          </li>
-          <li className={styles.tapPhoto}>
-            <a className={styles.linkPhoto}>
-              <span className={styles.subTapBar} />
-              <span className={styles.subTapText}>포토</span>
-            </a>
-          </li>
-          <li className={styles.tapSimilar}>
-            <a className={styles.linkSimilar}>
-              <span className={styles.subTapBar} />
-              <span className={styles.subTapText}>함께 찾은 품종</span>
-            </a>
-          </li>
+          {subTapArray.map((subTapInfo, idx) => {
+            return (
+              <li className={styles.subTap} key={idx}>
+                <a className={styles.subTapLink}>
+                  <span className={styles.subTapText}>{subTapInfo}</span>
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
