eb582978255879f8abfbd0a53576df5dd2de5671
itemgiver <williamgyuhosuh@naver.com>
2021-11-23 16:13:58 +0900

    Fix: load style.css using express static

--- webServer/webServer.js
+++ webServer/webServer.js
@@ -1,54 +1,54 @@
-const axios = require("axios")
-const express = require("express")
-const cors = require("cors")
-const path = require("path")
-const config = require("config")
-const proxy = require("express-http-proxy")
-const bodyParser = require('body-parser')
+const axios = require("axios");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const config = require("config");
+const proxy = require("express-http-proxy");
+const bodyParser = require("body-parser");
 
-const port = config.get("webServerPort")
-const renderServerAddress = config.get("renderServerAddress")
-const payloadSelector = require("./payloadSelector")
-const { UrlShortener } = require("./openApi")
+const port = config.get("webServerPort");
+const renderServerAddress = config.get("renderServerAddress");
+const payloadSelector = require("./payloadSelector");
+const { UrlShortener } = require("./openApi");
 
-
-const app = express()
-app.use(cors())
-app.use("/proxy", proxy(renderServerAddress))
-app.use(bodyParser.urlencoded({ extended: true }))
+const app = express();
+app.use(cors());
+app.use(express.static("dist"));
+app.use("/proxy", proxy(renderServerAddress));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => {
-  let keys = req.query.keys
+  let keys = req.query.keys;
   if (keys !== undefined) {
-    keys = keys.split(",")
+    keys = keys.split(",");
   }
 
-  payload = payloadSelector.select(keys)
+  payload = payloadSelector.select(keys);
 
   const options = {
-    "url": renderServerAddress,
-    "method": "POST",
-    "data": payload
+    url: renderServerAddress,
+    method: "POST",
+    data: payload,
   };
 
   axios(options)
     .then((axiosRes) => {
       res.send(axiosRes.data);
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
 app.post("/open-apis/url-shortener", (req, res, next) => {
-  let url = req.body.url
+  let url = req.body.url;
   UrlShortener.do(url)
-    .then(axiosRes => {
+    .then((axiosRes) => {
       res.send({
-        "url": axiosRes.data.result.url
-      })
+        url: axiosRes.data.result.url,
+      });
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
 app.listen(port, () => {
   console.log(`Web Server listening at http://localhost:${port}`);
-})
+});
