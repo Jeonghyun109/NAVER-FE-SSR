0ac4a0edb082d72a78043935975e9af3c190fc51
Billo Park <me@billo.io>
2021-11-23 15:05:55 +0900

    Add open API (#10)
    * Add open API
    
    * Add newline & remove unused log
    
    * Shortening catch phrase

--- webServer/webServer.js
+++ webServer/webServer.js
@@ -4,14 +4,18 @@ const cors = require("cors")
 const path = require("path")
 const config = require("config")
 const proxy = require("express-http-proxy")
+const bodyParser = require('body-parser')
 
 const port = config.get("webServerPort")
 const renderServerAddress = config.get("renderServerAddress")
 const payloadSelector = require("./payloadSelector")
+const { UrlShortener } = require("./openApi")
+
 
 const app = express()
 app.use(cors())
 app.use("/proxy", proxy(renderServerAddress))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res, next) => {
   let keys = req.query.keys
@@ -30,6 +34,17 @@ app.get("/", (req, res, next) => {
   axios(options)
     .then((axiosRes) => {
       res.send(axiosRes.data);
+    })
+    .catch(next)
+})
+
+app.post("/open-apis/url-shortener", (req, res, next) => {
+  let url = req.body.url
+  UrlShortener.do(url)
+    .then(axiosRes => {
+      res.send({
+        "url": axiosRes.data.result.url
+      })
     })
     .catch(next)
 })
