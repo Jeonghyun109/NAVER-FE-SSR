a956dae472612a08cdce91a5e447a30349a5123e
Billo Park <me@billo.io>
2021-11-23 15:03:11 +0900

    Add proxy (#12)
    * Add proxy
    
    * Change address to proxy
    
    * Reformat code

--- webServer/webServer.js
+++ webServer/webServer.js
@@ -1,36 +1,39 @@
-const axios = require("axios");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const path = require("path");
-const config = require("config");
-const port = config.get("webServerPort");
-const payloadSelector = require("./payloadSelector");
+const axios = require("axios")
+const express = require("express")
+const cors = require("cors")
+const path = require("path")
+const config = require("config")
+const proxy = require("express-http-proxy")
 
-app.use(cors());
-app.use(express.static("dist"));
+const port = config.get("webServerPort")
+const renderServerAddress = config.get("renderServerAddress")
+const payloadSelector = require("./payloadSelector")
+
+const app = express()
+app.use(cors())
+app.use("/proxy", proxy(renderServerAddress))
 
 app.get("/", (req, res, next) => {
-  let keys = req.query.keys;
+  let keys = req.query.keys
   if (keys !== undefined) {
-    keys = keys.split(",");
+    keys = keys.split(",")
   }
 
-  payload = payloadSelector.select(keys);
+  payload = payloadSelector.select(keys)
 
   const options = {
-    url: config.get("renderServerAddress"),
-    method: "POST",
-    data: payload,
+    "url": renderServerAddress,
+    "method": "POST",
+    "data": payload
   };
 
   axios(options)
     .then((axiosRes) => {
       res.send(axiosRes.data);
     })
-    .catch(next);
-});
+    .catch(next)
+})
 
 app.listen(port, () => {
   console.log(`Web Server listening at http://localhost:${port}`);
-});
+})
