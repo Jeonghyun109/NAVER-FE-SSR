4ef36c54b3be9285d96fff1ea7a6dbe0f25b1c12
itemgiver <williamgyuhosuh@naver.com>
2021-12-01 23:43:14 +0900

    Refactor: remove images.map duplicate code

--- renderServer/components/cultureMainCard/images.js
+++ renderServer/components/cultureMainCard/images.js
@@ -2,50 +2,28 @@ import React from "react";
 import styles from "./images.module.scss";
 
 function Images(props) {
-  //TODO image 개수 알려주는 thumbnail
   return (
     <div className={styles.imgdiv}>
       <ul className={styles.img_list}>
         {props.common.imageList.map((imgInfo, idx) => {
-          {
-            idx === 0 && (
-              <li key={idx}>
-                <a href={props.common.imgURL}>
-                  <img
-                    className={styles.imgFirst}
-                    src={imgInfo["src"]}
-                    alt={imgInfo["alt"]}
-                  />
-                </a>
-              </li>
-            );
-          }
-          {
-            idx === props.common.imageList.length - 1 && (
-              <li key={idx}>
-                <a href={props.common.imgURL}>
-                  <img
-                    className={styles.imgLast}
-                    src={imgInfo["src"]}
-                    alt={imgInfo["alt"]}
-                  />
-                </a>
-              </li>
-            );
-          }
-          {
-            idx !== 0 && idx !== props.common.imageList.length - 1 && (
-              <li key={idx}>
-                <a href={props.common.imgURL}>
-                  <img
-                    className={styles.img}
-                    src={imgInfo["src"]}
-                    alt={imgInfo["alt"]}
-                  />
-                </a>
-              </li>
-            );
-          }
+          const imgStyle =
+            idx === 0
+              ? styles.imgFirst
+              : idx === props.common.imageList.length - 1
+              ? styles.imgLast
+              : styles.img;
+
+          return (
+            <li key={idx}>
+              <a href={props.common.imgURL}>
+                <img
+                  className={imgStyle}
+                  src={imgInfo["src"]}
+                  alt={imgInfo["alt"]}
+                />
+              </a>
+            </li>
+          );
         })}
       </ul>
     </div>
--- renderServer/components/cultureMainCard/index.js
+++ renderServer/components/cultureMainCard/index.js
@@ -1,77 +1,13 @@
 import React from "react";
 import styles from "./index.module.scss";
 import Images from "./images";
+import BasicInfo from "./basicInfo";
 
 function CultureMainCard(props) {
-  function renderInformation(lst) {
-    var keys = Object.keys(lst);
-    var temp = [];
-    for (var i = 0; i < keys.length; i++) {
-      if (lst[keys[i]].link) {
-        temp.push(
-          <tr key={i}>
-            <td className={styles.infoHeading} key={"td" + i}>
-              {keys[i]}
-            </td>
-            <td key={"td" + i} className={styles.infoDetail}>
-              <a href={lst[keys[i]].url} className={styles.Link}>
-                {lst[keys[i]].detail}
-              </a>
-            </td>
-          </tr>
-        );
-      } else {
-        temp.push(
-          <tr key={i}>
-            <td className={styles.infoHeading} key={"td" + i}>
-              {keys[i]}
-            </td>
-            <td key={"td" + i} className={styles.infoDetail}>
-              {lst[keys[i]].detail}
-            </td>
-          </tr>
-        );
-      }
-    }
-    return temp;
-  }
-
   return (
     <div className={styles.bodyDiv}>
       <Images common={props.common} />
-      <div className={styles.infoArea}>
-        <a
-          href={props.common.descriptionURL}
-          className={styles.linkMiddleTitle}
-        >
-          <h3 className={styles.h3}>{props.common.title}</h3>
-          <div className={styles.arrow}>
-            <i className="fas fa-arrow-right"></i>
-          </div>
-        </a>
-        <div className={styles.details}>
-          <table className={styles.infoTable}>
-            <tbody>{renderInformation(props.common.information)}</tbody>
-          </table>
-        </div>
-        <div style={{ fontSize: "12pt", color: "#424242" }}>
-          {props.common.descriptionText}
-        </div>
-        <div className={styles.btn}>
-          <a href={props.common.place}>
-            <span className={styles.mapBtn}>
-              <i
-                className="fas fa-map-marker-alt"
-                style={{ color: "#06c755" }}
-              ></i>{" "}
-              지도
-            </span>
-          </a>
-          <span className={styles.shareBtn}>
-            <i className="fas fa-share-square"></i>
-          </span>
-        </div>
-      </div>
+      <BasicInfo common={props.common} />
     </div>
   );
 }
